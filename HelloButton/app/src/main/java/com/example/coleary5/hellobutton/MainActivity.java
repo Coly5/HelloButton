a09db3d50b429224e988b13efda1a16762b8854d
Coly5 <cahiroleary@gmail.com>
2018-10-23 11:18:47 +0100

    Update Hello Button

--- HelloButton/app/src/main/java/com/example/coleary5/hellobutton/MainActivity.java
+++ HelloButton/app/src/main/java/com/example/coleary5/hellobutton/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.coleary5.hellobutton;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mCounter = 0;
+    private TextView mMessageTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //This method is called when the app launches
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Code I add goes here :)
+        //connect code to our view
+        mMessageTextView = findViewById(R.id.message_textview);
+
+        final Button resetButton = findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCounter=0;
+                updateView();
+            }
+        });
+
+                //test - mMessageTextView.setText("Cahir");
+        //Log.d("HB", "Test log");
+
+        //Example crash
+       // mMessageTextView=findViewById(0);
+        //mMessageTextView.setText("This will crash");
+
+    }
+
+    public void handleDecrement(View view) {
+        mCounter = mCounter -1;
+        updateView();
+    }
+
+    public void handleIncrement(View view) {
+        mCounter = mCounter +1;
+        updateView();
+    }
+    private void updateView(){
+        mMessageTextView.setText(getString(R.string.message_format, mCounter));
+
     }
 }
